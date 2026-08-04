62923cdea47a73b93e1a0cd4a4e946950a634c8b
John Coonrod <john@mcld.org>
2026-08-04 15:33:22 -0400

    pyramid

--- pyramid/pyramid.js
+++ pyramid/pyramid.js
@@ -5,25 +5,35 @@ deal();
 function deal(){
     ndealt=0;
     for(i=0;i<7;i++){
-        for (j=1;j<i+1;j++){
-            appendCard(ndealt);
+        createRow(i); // create overlapping rows of cards
+        for (j=0;j<i+1;j++){
+            appendCard(ndealt,i,1); // add cards to the row
             ndealt++;
         }
     }
 }
-function appendCard(cardNo,j,up) { // add a card position i in the deck to the end of cascade j
-	cascade=document.getElementById("c"+j);
-	z=cascade.childElementCount;
+function createRow(i){ // create overlapping rows in the tableau
+    const tableau=document.getElementById("tableau");
+    console.log("createRow "+i);
+    row=document.createElement("div");
+    row.id="c"+i;
+    row.style.position='absolute';
+    row.style.width='100%';
+	y=(i+1)*5;
+    row.style.top=y.toString()+"vw";
+    row.style.textAlign="center";
+    tableau.appendChild(row);
+}
+function appendCard(cardNo,i,up) { // add a card to row i 
+	row=document.getElementById("c"+i);
     card=document.createElement("div");
     if(up) {card.innerHTML=cards[deck[cardNo]];
 		flips[cardNo]=1;}
 	else{card.innerHTML=back;flips[cardNo]=0;}
     card.id="v"+cardNo;
     card.classList.add("card");
-    card.style.position='absolute';
-    card.style.width='100%';
-	y=(z-1)*5;
-    card.style.top=y.toString()+"vw";
+    card.style.width="12vw";
+    card.style.display='inline-block';
 	if(up) card.setAttribute("onclick","tryMove(this);");
-    cascade.appendChild(card);
+    row.appendChild(card);
 }
