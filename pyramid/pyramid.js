2a83d0fb1a282410a9b76c5ab28ea12667308cba
John Coonrod <john@mcld.org>
2026-08-12 19:56:11 -0400

    next steps on pyramid

--- pyramid/pyramid.js
+++ pyramid/pyramid.js
@@ -1,14 +1,29 @@
+// NEXT STEPS
+// Paint one or two cards yellow when clicked.
+// Check value of the cards simply by looking into innerHTML?
 const ncards=52;
 var nlast=52; //this will decrease as we delete cards from the stack
 var ndealt=0;
+var nclicked=0;
+var click1; // which card was clicked first?
+var click2; // which card was clicked second?
 var lefts=[]; // squares which might coveer other squares
 var rights=[];
 const offsets=[1,2,2,3,3,3,4,4,4,4,5,5,5,5,5,6,6,6,6,6,6];
+const yellowrgb=[255,255,0];
 var tapped=0; // value+1 of first tapped? When it reaches 13 theygo.
 createCards();
 shuffle();
 deal();
-
+function pop(i){ // do various things when a card is clicked
+    console.log("pop i="+i+" nclicked="+nclicked);
+    // toggle background color between white and yellow adjust nclicked
+    card=document.getElementById("cd"+i);
+    const backgroundColor = card.style.backgroundColor;
+    console.log("backgroundColor="+backgroundColor+" ending in "+backgroundColor.slice(-2));
+    if(backgroundColor=="yellow") card.style.backgroundColor="white";
+        else card.style.backgroundColor="yellow";
+}
 function deal(){
     ndealt=0;
     for(i=0;i<7;i++){
@@ -25,42 +40,26 @@ function deal(){
     }
     for(i=21;i<28;i++){ // mark the bottom row as visible
         lefts[i]=-1;
-        right[i]=-1;
+        rights[i]=-1;
     }
 }
 
 function next(){
     console.log("Next "+ndealt);
     const cardno=deck[ndealt];
-    s='<div class="card" onclick="testflip('+cardno+')">'+cards[cardno]+'</dev>';
-    document.getElementById("flip").innerHTML=s;
+    s='<div id="cd28" class="card" style="backgroundColor: white;">'
+    +cards[cardno]+'</dev>';
+    document.getElementById("v28").innerHTML=s;
     ndealt++;
     if(ndealt==nlast) ndealt=28;
 }
 
-function testflip(cardno){
-    val=getVal(cardno); // rangers from 0 Ace to 12 king
-    if(val==12) {
-        document.getElementById("flip").innerHTML="";
-        nuke(cardno); //eliminate this card from cards and deck both; 
-    }
-}
 function nuke(cardno){
     cards.splice(cardno-1,1); // one card removed
     i=deck.indexOf(cardno); // which deck item to remove
     deck.splice(i-1,1);
     nlast=deck.length; // this should be 1 less 
     console.log("Nuke "+cardno+" i="+i+" nlast="+nlast);
-}
-function pop(i){
-    console.log("pop i="+i);
-    blank=document.createElement("div");
-    blank.id="v"+i;
-    blank.classList.add("blank");
-    blank.style.width="12vw";
-    blank.style.display='inline-block';
-    blank.style.top='0';
-    document.getElementById("v"+i).replaceWith(blank);
 }
 
 function clearBoard(){
@@ -80,6 +79,7 @@ function appendCard(i) { // add a card to cell vi
     card.id="cd"+i;
     card.classList.add("card");
     card.style.width="12vw";
+    card.style.backgroundColor="white";
     card.style.display='inline-block';
     card.style.top='0';
     card.innerHTML=cards[deck[i]];
